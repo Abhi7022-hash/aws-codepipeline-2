8ce9b92038720370e8560cd841dcf5487cf164e8
abhim7022 <abhishekdindavar049@gmail.com>
2025-08-23 12:31:38 +0530

    Create start_container.sh

---
+++ scripts/start_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull abhishek661/simple-python-flask-app
+# Run the Docker image as a container
+docker run -d -p 5000:5000 abhishek661/simple-python-flask-app
